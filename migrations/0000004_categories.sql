4dc94918f470393d9ea47e4c3482e41bd9331438
slavikyd <slavikyd@gmail.com>
2026-05-11 16:22:59 +0300

    revert of accident changes due to merge

--- migrations/0000004_categories.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE categories (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name text NOT NULL UNIQUE
-);
--- migrations/0000006_orders_items_comment.sql
+++
@@ -1,2 +0,0 @@
--- Per line-item comment from cashier (stored at order time; not shown in admin UIs yet).
-ALTER TABLE orders_items ADD COLUMN IF NOT EXISTS comment text;
