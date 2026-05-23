e13ecb79c6eae22544618ed4716e80f25fad9931
slavikyd <slavikyd@gmail.com>
2026-05-23 18:29:12 +0300

    deleted deprecated mention of admin_routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,3 @@
-from . import admin_routes, crud_routes, extra_routes
+from . import crud_routes, extra_routes
 
-__all__ = ['admin_routes', 'crud_routes', 'extra_routes']
+__all__ = ['crud_routes', 'extra_routes']
