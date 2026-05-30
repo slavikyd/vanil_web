95976c83a8bd4864938d4edcea88643b395170c8
slavikyd <slavikyd@gmail.com>
2026-05-30 18:48:00 +0300

    fix migration issue

--- migrations/0000007_indexes.sql
+++ migrations/0000007_indexes.sql
@@ -1,4 +1,4 @@
-CREATE INDEX idx_orders_order_for ON orders(order_for DESC);
-CREATE INDEX idx_orders_cashier_id ON orders(cashier_id);
-CREATE INDEX idx_orders_shop_id ON orders(shop_id);
-CREATE INDEX idx_orders_items_order_id ON orders_items(order_id);
+CREATE INDEX IF NOT EXISTS idx_orders_order_for ON orders(order_for DESC);
+CREATE INDEX IF NOT EXISTS idx_orders_cashier_id ON orders(cashier_id);
+CREATE INDEX IF NOT EXISTS idx_orders_shop_id ON orders(shop_id);
+CREATE INDEX IF NOT EXISTS idx_orders_items_order_id ON orders_items(order_id);
